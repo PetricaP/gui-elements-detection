b16139b0db238d183e18694f6ead7abfd3ee3c2c
ppetrica <ppetrica@bitdefender.com>
2019-12-16 18:39:13 +0200

    Removed the need to specify model path and image path for gui mode.

--- gui_analyzer.py
+++ gui_analyzer.py
@@ -9,14 +9,18 @@ from application import Application, analyze_image
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('model_path', help='Path to the pb file for the neural network')
-    parser.add_argument('image_path', help='Path to the image to analyze')
+    parser.add_argument('--model_path', '-m', help='Path to the pb file for the neural network',
+                        default='other\\frozen_east_text_detection.pb')
+    parser.add_argument('--image_path', '-i', help='Path to the image to analyze')
     parser.add_argument('--output', '-o', help='Path to the results file to write to', default='results.json')
     parser.add_argument('--gui', action='store_true', help='Launch GUI for application')
     parser.add_argument('--debug', action='store_true', help='Store intermediate results in debug.json')
     args = parser.parse_args()
 
     if not args.gui:
+        if not args.model_path or not args.image_path:
+            parser.error('The following parameters are required for non-gui mode: --model_path, --image_path')
+
         image = cv2.imread(args.image_path)
         results = analyze_image(image, args.model_path)
 
